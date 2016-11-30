b20003e2140b68b26672f477e95dc4da89c3c509
Alexis Rosuel <Alexis Rosuel>
2016-11-30 12:13:02 +0100

    Implemented skin color likelihood

--- src/contour.py
+++ src/contour.py
@@ -5,3 +5,18 @@ Created on Tue Nov 29 12:13:17 2016
 @author: Alexis
 """
 
+def facial_contour(vector_likelihood):
+    """ Calcule la valeur du likelihood d'un pixel.
+    La distribution utilisée est une loi normale bi-dimensionnelle, où
+    la moyenne et la matrice de variance covariance est fixée (valeurs 
+    classique qui fonctionnent bien)
+    
+    Args:
+    	matrice numpy de dimension 1 (valeur du likelihood) 
+    	
+    Returns:
+    	matrice numpy de dimension 0 (valeur du likelihood)
+    """
+    
+    # skin color threshold value
+    threshold = 10
