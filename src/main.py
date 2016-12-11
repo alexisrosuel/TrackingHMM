f497a75a3970ca0849fa727aa21ffe67ee1b7cf9
Pascal <jauffret@etud.insa-toulouse.fr>
2016-12-11 18:07:05 +0100

    Integrated likelihood evaluation into main script, and encoded weihgt updating

--- src/main.py
+++ src/main.py
@@ -11,12 +11,15 @@ import sys, argparse,glob
 import skimage.io as io
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Ellipse
+from matplotlib.patches import Ellipse, Circle
 
-
+from observation import evaluate
 
 """
 Execute main script
+
+Exemple of execution :
+python3 main.py --source "sequence1"
 
 """
 
@@ -27,16 +30,16 @@ def main(argv):
 	args = parser.parse_args()
 	source = args.source
 	
+	#TODO => passer cette valeur en argument du script ?
+	particle_number = 400
 
 
 	if(source != "webcam"):
 		file_list = glob.glob("../data/" + source + "/*")
 		print(file_list)
 		
-		#Get shape of picture to initialize weights
-		particle_number = 500
-		weights = np.array([(1.0/particle_number) for x in range(particle_number)])
-		print(weights)
+		
+		
 
 
 		for file_picture in sorted(file_list):
@@ -58,22 +61,36 @@ def main(argv):
 
 			
 
-			'''
-			Update weights
+
+			#Random data example to show you the structure
+			particles = []
+			for _ in range(particle_number):
+				particle = {}
+				#X ET Y INVERSE POUR POUVOIR PLOT CORRECTEMENT
+				particle["x"] = int(np.random.rand(1) * array_picture.shape[0])
+				particle["y"] = int(np.random.rand(1) * array_picture.shape[1])
+				particle["weight"] = np.random.rand(1)
+
+				particles.append(particle)
+
 
 			'''
+			Compute the likelihood of each particle
 
-			# weights = update_weights(particles, array_picture)
+			Find the best cercle of each particle
 
+			'''
+			particles = evaluate(array_picture, particles)
 
 
 
 			'''
-			Evaluate
+			Update and normalize weights with their likelihood
 
 			'''
 
-			#ellipse = evaluate(array_picture, particles, weight ...)
+			particles = update_weights(particles)
+			
 
 
 			'''
@@ -81,27 +98,23 @@ def main(argv):
 
 			Plot particle proportionally to weights
 
-			Plot ellipse
+			Plot ellipse/circle
 
 			'''
 			
 
-			#Random data example to show you the structure
-			particles = {}
-			particles["x"] = np.random.rand(particle_number) * array_picture.shape[1]
-			particles["y"] = np.random.rand(particle_number) * array_picture.shape[0]
-
-			particles["weights"] = np.random.rand(particle_number)
 			
-
+			
+			'''
 			ellipse = {}
 			ellipse["x"] = np.random.rand(1) * array_picture.shape[0]
 			ellipse["y"] = np.random.rand(1) * array_picture.shape[0]
-                    ellipse["angle"] = np.random.randint(181)
+            ellipse["angle"] = np.random.randint(181)
 			ellipse["height"] = np.random.randint(50,150)
+			'''
 
 			#Display picture/particles/ellipse
-			display_picture(picture=array_picture, ellipse=ellipse, particles=particles, weights=weights)
+			display_picture(picture=array_picture, particles=particles)
 
 
 
@@ -114,7 +127,7 @@ def main(argv):
 	
 		
 
-def display_picture(picture ,ellipse, particles, weights):
+def display_picture(picture ,particles):
 	
 	
 	fig,ax = plt.subplots(1)
@@ -127,17 +140,53 @@ def display_picture(picture ,ellipse, particles, weights):
 	ax.autoscale(False)
 
 	#Plot particles with weights
-	ax.scatter(particles["x"], particles["y"], marker="x", s=weights*100 )
+	for particle in particles:
 
-	#Add ellipse
-	ells = Ellipse(xy=ellipse["xy"], width=20, height=ellipse["height"], angle=ellipse["angle"])
-	ax.add_patch(ells)
-	
+		#X ET Y inverse pour pouvoir plot correctement
+		ax.scatter(particle["y"], particle["x"], marker="x", s=particle["weight"]*100 )
+
+
+
+		'''
+		#Add ellipse
+		ells = Ellipse(xy=ellipse["xy"], width=20, height=ellipse["height"], angle=ellipse["angle"])
+		ax.add_patch(ells)
+		'''
+
+		#Add circle if particle has key best_cercle
+		if "best_cercle" in particle:
+			#Add circle
+			#ENCORE INVERSE X ET Y 
+			circle = particle["best_cercle"]
+			circle_patch = Circle(xy=(circle["c"], circle["r"]), radius=circle["radius"])
+			circle_patch.set_facecolor("None")
+			circle_patch.set_edgecolor("red")
+			ax.add_patch(circle_patch)
+		
 	
 	plt.show()
 	
 
 
+##TODO : optimiser cette fonction
+'''
+Fixe les poids des particules en fonction de leur likelihood, et normalise pour avoir une somme égale a 1.
+
+Args:
+    dictionnaire de particle (clé utilisée : "likelihood" et "weight")
+    	
+Returns:
+	dictionnaire de particle avec clé "weight" mise à jour
+'''
+def update_weights(particles):
+	normalizing_constant = sum([particle["likelihood"] for particle in particles])
+
+	for particle in particles :
+		particle["weight"] = particle["likelihood"] / normalizing_constant 
+		print(particle["weight"])
+		print("\n")
+	
+	return particles
 
 
 if __name__ == "__main__":
